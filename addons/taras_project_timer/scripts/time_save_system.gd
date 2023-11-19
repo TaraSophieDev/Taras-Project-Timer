41c08693ffab6a121a1f71bcda9be32f4ed4ee99
TerraDev64 <TerraDev64@gmail.com>
2023-11-19 21:50:13 +0100

    added delete save and changed back to float

--- addons/taras_project_timer/scripts/time_save_system.gd
+++ addons/taras_project_timer/scripts/time_save_system.gd
@@ -7,17 +7,17 @@ var settings_path: String = "user://settings.save"
 func save_time(time: float):
 	var save_time = FileAccess.open(time_save_path, FileAccess.WRITE)
 	#var json = JSON.stringify(save_time())
-	#save_time.store_64(json)
+	save_time.store_float(time)
 	
 func save_settings():
 	pass
 
 func load_time():
 	if not FileAccess.file_exists(time_save_path):
-		return
+		return 0.0
 	
 	var save_time = FileAccess.open(time_save_path, FileAccess.READ)
-	var time: float = save_time.get_64()
+	var time: float = save_time.get_float()
 	save_time.close()
 	print(time)
 	return time
@@ -26,4 +26,6 @@ func load_settings():
 	pass
 
 func delete_time_save():
-	pass
+	if not FileAccess.file_exists(time_save_path):
+		return
+	DirAccess.remove_absolute(time_save_path)
