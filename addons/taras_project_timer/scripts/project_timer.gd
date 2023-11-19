b0ded57553af3d6e51fe636b8fc9660215ea1658
TerraDev64 <TerraDev64@gmail.com>
2023-11-19 21:51:00 +0100

    changed formatting and added focus check

--- addons/taras_project_timer/scripts/project_timer.gd
+++ addons/taras_project_timer/scripts/project_timer.gd
@@ -4,14 +4,17 @@ extends PanelContainer
 var _save_manager = SaveManager.new()
 
 var time_started: bool = false
-var time: int = 0.0
+var pause_on_unfocus: bool = false
+var is_focused: bool = false
+var time: float = 0.0
 var render_timeout: float = 1.0
 
 func convert_time_to_readable():
-	var seconds = fmod(time, 60)
-	var minutes = fmod(time, 60*60) / 60
-	var hours = fmod(time, 60*60*60) / 60
-	var translated_time = "%02d:%02d:%02d" % [hours, minutes, seconds]
+	var seconds:int = ((int(time) % 86400) % 3600) % 60
+	var minutes: int = ((int(time) % 86400) % 3600) / 60
+	var hours: int = (int(time) % 86400) / 3600
+	var days: int = int(time) / 86400
+	var translated_time = "%02d:%02d:%02d:%02d" % [days ,hours, minutes, seconds]
 	
 	$VBoxContainer/TimeLabel.text = "" + str(translated_time)
 	
@@ -21,10 +24,11 @@ func reset_time():
 
 func _ready():
 	time = _save_manager.load_time()
+	pass
 
 
 func _process(delta):
-	if time_started:
+	if time_started and (not pause_on_unfocus or pause_on_unfocus and is_focused):
 		time += delta
 	render_timeout -= delta
 	if render_timeout < 0.0:
@@ -34,6 +38,7 @@ func _process(delta):
 
 func _on_start_button_pressed():
 	time_started = true
+	pass
 
 
 func _on_stop_button_pressed():
@@ -44,3 +49,14 @@ func _on_stop_button_pressed():
 func _on_reset_button_pressed():
 	reset_time()
 	_save_manager.delete_time_save()
+
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_APPLICATION_FOCUS_IN:
+		is_focused = true
+	elif what == MainLoop.NOTIFICATION_APPLICATION_FOCUS_OUT:
+		is_focused = false
+
+
+func _on_focus_check_box_toggled(toggled_on):
+	pause_on_unfocus = toggled_on
